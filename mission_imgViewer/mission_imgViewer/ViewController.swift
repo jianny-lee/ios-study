2592d36342b0017bb9ec452d36800b4c3678047b
jianny-lee <56791347+jianny-lee@users.noreply.github.com>
2019-12-15 15:05:58 +0900

    update viewController code

--- mission_imgViewer/mission_imgViewer/ViewController.swift
+++ mission_imgViewer/mission_imgViewer/ViewController.swift
@@ -14,20 +14,41 @@ class ViewController: UIViewController {
     @IBOutlet var imgView: UIImageView!
     @IBOutlet var preButton: UIButton!
     @IBOutlet var nextButton: UIButton!
+    
     //variable 선언
-    var imgArray;[6]:UIImage
+    var num : Int = 0
+    //image file의 이름을 저장할 변수 선언
+    var imageArr : [String] = ["01.png","02.png","03.png","04.png","05.png","06.png"]
+    var tmpImage : UIImage? //view상에 보여줄 image
     
     
     //action function 선언
+    //previous button 선택
     @IBAction func preButtonAction(_ sender: UIButton) {
+        if(num == 0){
+            num = imageArr.count - 1 //array의 크기
+        } else {
+            num -= 1
+        }
+        tmpImage = UIImage(named: imageArr[num])
+        imgView.image = tmpImage
     }
+    //next button 선택
     @IBAction func nextButtonAction(_ sender: UIButton) {
+        if(num == imageArr.count - 1){
+            num = 0
+        } else {
+            num += 1
+        }
+        tmpImage = UIImage(named: imageArr[num])
+        imgView.image = tmpImage
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        firstImg = UIImage(named: "01.png")
+        tmpImage = UIImage(named: imageArr[num]) //tmpImage에 첫번째 사진을 입력
+        imgView.image = tmpImage // imageView에 입력
     }
 
 
